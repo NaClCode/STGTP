63a5e1e386f2251b6a528363e204f82286242610
NaCl <naclcode@qq.com>
2025-12-03 23:09:25 +0800

    Update trainval.py

--- trainval.py
+++ trainval.py
@@ -21,7 +21,7 @@ def get_parser():
     parser.add_argument('--P',  default=True, type=ast.literal_eval)
     parser.add_argument('--S',  default=True, type=ast.literal_eval)
     
-    parser.add_argument('--model', default=['STGTP'])
+    parser.add_argument('--model', default='STGTP')
     parser.add_argument('--dataset')
     parser.add_argument('--phase', default='train', choices=['train', 'test'])
     
